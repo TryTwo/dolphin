bbb07d75f084688c9f009d95d791b9b4b3936136
TryTwo <taolas@gmail.com>
2024-09-17 23:29:13 -0700

    Add GFX property tabs to game properties window, allowing them to be set to the user game ini. Additionally, refactor ConfigWidgets to reduce duplication. Refactor GameConfigWidget to use config system.
    Creates a layer outside the game config layer system and passes it to the created gfx widows, so as to not interfere with the global config system.
    
    Supports multiple game properties being open at once.
    Supports editing while a game is playing, but the options only save and update the active game when the window is closed.
    Right-clicking will remove a property from the game ini.

--- Source/Core/DolphinQt/Config/ConfigControls/ConfigChoice.h
+++ Source/Core/DolphinQt/Config/ConfigControls/ConfigChoice.h
@@ -7,35 +7,47 @@
 #include <utility>
 #include <vector>
 
+#include "DolphinQt/Config/ConfigControls/ConfigControl.h"
 #include "DolphinQt/Config/ToolTipControls/ToolTipComboBox.h"
 
-#include "Common/Config/Config.h"
+namespace Config
+{
+template <typename T>
+class Info;
+}  // namespace Config
 
-class ConfigChoice : public ToolTipComboBox
+class ConfigChoice final : public ConfigControl<ToolTipComboBox>
 {
   Q_OBJECT
 public:
-  ConfigChoice(const QStringList& options, const Config::Info<int>& setting);
+  ConfigChoice(const QStringList& options, const Config::Info<int>& setting,
+               Config::Layer* layer = nullptr);
+
+protected:
+  void OnConfigChanged() override;
 
 private:
   void Update(int choice);
 
   Config::Info<int> m_setting;
+  Config::Layer* m_layer = nullptr;
 };
 
-class ConfigStringChoice : public ToolTipComboBox
+class ConfigStringChoice final : public ConfigControl<ToolTipComboBox>
 {
   Q_OBJECT
 public:
   ConfigStringChoice(const std::vector<std::string>& options,
-                     const Config::Info<std::string>& setting);
+                     const Config::Info<std::string>& setting, Config::Layer* layer = nullptr);
   ConfigStringChoice(const std::vector<std::pair<QString, QString>>& options,
-                     const Config::Info<std::string>& setting);
+                     const Config::Info<std::string>& setting, Config::Layer* layer = nullptr);
+  void Load();
+
+protected:
+  void OnConfigChanged() override;
 
 private:
-  void Connect();
   void Update(int index);
-  void Load();
 
   Config::Info<std::string> m_setting;
   bool m_text_is_data = false;
--- Source/Core/DolphinQt/Config/ConfigControls/ConfigSlider.h
+++ Source/Core/DolphinQt/Config/ConfigControls/ConfigSlider.h
@@ -3,20 +3,27 @@
 
 #pragma once
 
+#include "DolphinQt/Config/ConfigControls/ConfigControl.h"
 #include "DolphinQt/Config/ToolTipControls/ToolTipSlider.h"
 
 namespace Config
 {
 template <typename T>
 class Info;
-}
+}  // namespace Config
 
-class ConfigSlider : public ToolTipSlider
+class ConfigSlider final : public ConfigControl<ToolTipSlider>
 {
   Q_OBJECT
 public:
   ConfigSlider(int minimum, int maximum, const Config::Info<int>& setting, int tick = 0);
+  ConfigSlider(int minimum, int maximum, const Config::Info<int>& setting,
+               Config::Layer* target_layer, int tick = 0);
+
   void Update(int value);
+
+protected:
+  void OnConfigChanged() override;
 
 private:
   const Config::Info<int>& m_setting;
